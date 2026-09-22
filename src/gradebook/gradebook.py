30f3ec8427e520b8793d9f1716699801d08ad3b3
Abdul Baseer <abdulbaseerdeho@gmail.com>
2026-09-22 15:28:07 +0500

    gradebook.py modified

---
+++ src/gradebook/gradebook.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+        self.scores = []
+
+    def add_score(self, score):
+        self.scores.append(score)
+        
